0c13455c45a3cbe43dd6b9e35f578f6c3045ee84
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2025-03-22 09:45:45 -0700

    Add redirect

--- src/providers/ApplicationProvider.php
+++ src/providers/ApplicationProvider.php
@@ -30,13 +30,24 @@ class ApplicationProvider implements Hiraeth\Provider
 		$base_path = rtrim($app->getEnvironment('BASE_PATH', ''), '/');
 		$url_path  = parse_url((string) $_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-		if (str_starts_with($url_path, $base_path)) {
-			$_SERVER['REQUEST_URI'] = str_replace(
-				$url_path,
-				substr($url_path, strlen($base_path)),
-				$_SERVER['REQUEST_URI']
-			);
+		if ($base_path) {
+			if (str_starts_with($url_path, $base_path . '/')) {
+				$_SERVER['REQUEST_URI'] = str_replace(
+					$url_path,
+					substr($url_path, strlen($base_path)),
+					$_SERVER['REQUEST_URI']
+				);
+
+			} else {
+				header('Location: ' . str_replace(
+					$_SERVER['REQUEST_URI'],
+					$url_path,
+					$base_path . $url_path
+				), TRUE, 301);
+				exit(301);
+			}
 		}
+
 
 		if (!$app->has(UrlGenerator::class)) {
 			$app->get(Hiraeth\Broker::class)->alias(UrlGenerator::class, DefaultUrlGenerator::class);
